81933fa3470a32b3ecd4b36fc11b622d8d348023
josep ramon <jrcamps@gmail.com>
2015-01-30 20:59:31 +0100

    Update the route name
    The entity name should be plural, but 'medias' looks weird. And ugly too.

--- routes/media.js
+++ routes/media.js
@@ -3,9 +3,9 @@ module.exports = function(router) {
 
   var Media = require('../models/Media');
 
-  router.route('/medias')
+  router.route('/media')
 
-    // get all the medias (accessed at GET http://localhost:port/api/medias)
+    // get all the media (accessed at GET http://localhost:port/api/media)
     .get(function(req, res) {
       Media.find(function(err, models) {
         if (err)
@@ -15,7 +15,7 @@ module.exports = function(router) {
     })
 
 
-    // create a media (accessed at POST http://localhost:port/api/medias)
+    // create a media (accessed at POST http://localhost:port/api/media)
     .post(function(req, res) {
 
       // create a new instance of the Media model
@@ -37,9 +37,9 @@ module.exports = function(router) {
     });
 
 
-  router.route('/medias/:media_id')
+  router.route('/media/:media_id')
 
-    // get the media with that id (accessed at GET http://localhost:port/api/medias/:media_id)
+    // get the media with that id (accessed at GET http://localhost:port/api/media/:media_id)
     .get(function(req, res) {
       Media.findById(req.params.media_id, function(err, model) {
         if (err)
@@ -49,7 +49,7 @@ module.exports = function(router) {
     })
 
 
-    // update the media with this id (accessed at PUT http://localhost:port/api/medias/:media_id)
+    // update the media with this id (accessed at PUT http://localhost:port/api/media/:media_id)
     .put(function(req, res) {
 
       // use our media model to find the media we want
@@ -74,7 +74,7 @@ module.exports = function(router) {
     })
 
 
-    // delete the media with this id (accessed at DELETE http://localhost:port/api/medias/:media_id)
+    // delete the media with this id (accessed at DELETE http://localhost:port/api/media/:media_id)
     .delete(function(req, res) {
       Media.remove({
         _id: req.params.media_id
