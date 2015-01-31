7ade08916a21118a8c3f798d58f5fb085f943574
josep ramon <jrcamps@gmail.com>
2015-01-31 01:33:30 +0100

    Cleanup

--- app.js
+++ app.js
@@ -79,6 +79,3 @@ app.use('/api', router);
 // =============================================================================
 app.listen(port);
 console.log('Server running on port ' + port);
-
-
-//http://scotch.io/tutorials/javascript/build-a-restful-api-using-node-and-express-4
