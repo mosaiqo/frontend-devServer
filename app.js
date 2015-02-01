32cbb424be01a39602e8812819d12e9d2d587109
josep ramon <jrcamps@gmail.com>
2015-02-01 18:39:30 +0100

    Modularise the testing server

--- app.js
+++ app.js
@@ -2,21 +2,19 @@
 /* jshint -W097 */
 'use strict';
 
-
-// BASE SETUP
-// =============================================================================
 var
   fs         = require('fs'),
   express    = require('express'),
   bodyParser = require('body-parser'),
-  mongoose   = require('mongoose'),
 
   rootDir    = __dirname + '/../../',
   publicDir  = rootDir + 'dist/',
-  routesDir  = __dirname + '/routes/',
   port       = process.env.PORT || 5000,
   app        = express();
 
+
+// BASE SETUP
+// =============================================================================
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -32,47 +30,13 @@ app.get('/', function(req, res) {
 });
 
 
-// MONGO CONF.
+// FAKE API (the real on will be implemented using Laravel)
 // =============================================================================
-var mongoConfigParser = require('./lib/mongoConfigParser');
+var API = require('./modules/api');
 
-var mongoConn = new mongoConfigParser()
-  .setEnvDir( rootDir + 'db/mongo/env' )
-  .getConnectionString();
-
-// connect
-mongoose.connect(mongoConn);
-
-
-
-// API ROUTES
-// =============================================================================
-
-var router = express.Router();
-
-// middleware to use for all requests
-router.use(function(req, res, next) {
-  next(); // make sure we go to the next routes and don't stop here
-});
-
-// test route to make sure everything is working (accessed at GET http://localhost:PORT/api)
-router.get('/', function(req, res) {
-  res.json({ message: 'hooray! welcome to our api!' });
-});
-
-
-// load the routes
-fs.readdirSync(routesDir).forEach(function(file) {
-  var route = routesDir + file.substr(0, file.indexOf('.'));
-  console.log('Adding route:' + route);
-  require(route)(router);
-});
-
-
-// REGISTER THE ROUTES
+// Register the routes
 // all of our routes will be prefixed with /api
-app.use('/api', router);
-
+app.use('/api', API);
 
 
 // START THE SERVER
---
+++ modules/api/index.js
@@ -0,0 +1,48 @@
+/* global require, module, process, console, __dirname */
+/* jshint -W097 */
+'use strict';
+
+var
+  fs         = require('fs'),
+  express    = require('express'),
+  mongoose   = require('mongoose'),
+  rootDir    = __dirname + '/../../../../',
+  routesDir  = __dirname + '/routes/';
+
+
+// MONGO CONF.
+// =============================================================================
+var mongoConfigParser = require('../../lib/mongoConfigParser');
+
+var mongoConn = new mongoConfigParser()
+  .setEnvDir( rootDir + 'db/mongo/env' )
+  .getConnectionString();
+
+// connect
+mongoose.connect(mongoConn);
+
+
+// API ROUTES
+// =============================================================================
+var router = express.Router();
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+  next(); // make sure we go to the next routes and don't stop here
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:PORT/api)
+router.get('/', function(req, res) {
+  res.json({ message: 'hooray! welcome to our api!' });
+});
+
+
+// load the routes
+fs.readdirSync(routesDir).forEach(function(file) {
+  var route = routesDir + file.substr(0, file.indexOf('.'));
+  console.log('Adding route:' + route);
+  require(route)(router);
+});
+
+
+module.exports = router;
