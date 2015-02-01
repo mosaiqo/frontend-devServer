43341a050f6f9500228f69987492e8f65c81586a
josep ramon <jrcamps@gmail.com>
2015-02-01 16:58:47 +0100

    Add media fixtures

---
+++ test/fixtures/media.js
@@ -0,0 +1,20 @@
+module.exports = (function() {
+  'use strict';
+
+  var
+    faker = require('faker'),
+    data = [];
+
+
+  for(var i=0, l=100; i < l; i++) {
+    data.push({
+      "name"        : faker.lorem.words(Math.floor(Math.random() * 4) + 1).join(' '),
+      "description" : faker.lorem.sentence(),
+      "url"         : faker.image.cats(),
+      "active"      : Math.random() > .5
+    });
+  }
+
+  return data;
+
+})();
