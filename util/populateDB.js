3abeac258e90353b890e5d66955db730d02bcbb1
josep ramon <jrcamps@gmail.com>
2015-02-07 12:00:04 +0100

    debug(build): Debugging travis mongo connection issues

--- util/populateDB.js
+++ util/populateDB.js
@@ -66,6 +66,11 @@ var
   loader = require('pow-mongodb-fixtures').connect(mongoConn.getConnectionString(), mongoConn.getConnectionOptions()),
   data   = {};
 
+
+console.log('Populating DB');
+console.log('----------------------------------');
+console.log(mongoConn.getConnectionString());
+
 // load the fixtures from the filesystem
 fs.readdirSync(fixturesDir).forEach(function(file) {
 
