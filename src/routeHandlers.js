5474ccc2bbfd273d155f5b3c50de99274870b4ff
josep ramon <jrcamps@gmail.com>
2015-02-08 20:28:40 +0100

    fix(app): replace sendfile with sendFile

--- src/routeHandlers.js
+++ src/routeHandlers.js
@@ -2,10 +2,13 @@
 /* jshint -W097 */
 'use strict';
 
-var publicDir = process.env.appPublicDir;
+var
+  fs        = require('fs'),
+  express   = require('express'),
+  publicDir = process.env.appPublicDir;
 
 module.exports = {
   root: function(req, res) {
-    res.sendfile(publicDir + '/index.html');
+    res.sendFile(publicDir + '/index.html');
   }
 };
