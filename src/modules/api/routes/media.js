8bf43be0c3ef86ac0580d4b54c681a3cfb4d1ac8
josep ramon <jrcamps@gmail.com>
2015-04-01 20:09:30 +0200

    doc: Add api routes documentation

--- src/modules/api/routes/media.js
+++ src/modules/api/routes/media.js
@@ -21,20 +21,22 @@ module.exports = function(router) {
      *
      * @apiSuccessExample {json} Success-Response:
      *     HTTP/1.1 200 OK
-     *     {
-     *       "_id": "54f1a3dc2d4714c77f4d8bce",
-     *       "name": "corporis incidunt est labore",
-     *       "description": "necessitatibus enim cupiditate ex ullam autem hic natus nihil nostrum",
-     *       "url": "http://lorempixel.com/640/480/cats",
-     *       "active": true
-     *     },
-     *     {
-     *       "_id": "54f1a3dc2d4714c77f4d8bcf",
-     *       "name": "ex nisi",
-     *       "description": "tenetur at et hic alias id iusto et repudiandae soluta",
-     *       "url": "http://lorempixel.com/640/480/cats",
-     *       "active": true
-     *     }
+     *     [
+     *       {
+     *         "_id": "54f1a3dc2d4714c77f4d8bce",
+     *         "name": "corporis incidunt est labore",
+     *         "description": "necessitatibus enim cupiditate ex ullam autem hic natus nihil nostrum",
+     *         "url": "http://lorempixel.com/640/480/cats",
+     *         "active": true
+     *       },
+     *       {
+     *         "_id": "54f1a3dc2d4714c77f4d8bcf",
+     *         "name": "ex nisi",
+     *         "description": "tenetur at et hic alias id iusto et repudiandae soluta",
+     *         "url": "http://lorempixel.com/640/480/cats",
+     *         "active": true
+     *       }
+     *     ]
      */
     .get(function(req, res, next) {
       Media.find(function(err, models) {
@@ -50,7 +52,24 @@ module.exports = function(router) {
     })
 
 
-    // create a media (accessed at POST http://localhost:port/api/media)
+    /**
+     * @api {post} /media Create a media object
+     * @apiName Create
+     * @apiGroup Media
+     *
+     * @apiExample Example usage:
+     * curl -4 -i -X POST http://localhost:9000/api/media --data "name=ItemName&description=ItemDescription&url=http%3A%2F%2Florempixel.com%2F640%2F480%2Fcats&active=true" --header "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1NGVlNjE3NTQ2NWVhZWUzNWNkMjM3ZWQiLCJpYXQiOjE0Mjc4MTczNTksImV4cCI6MTQyNzgyMDk1OX0.M3BboY6U9RJlX1ulVG7e9xRVrVdY3qVhvp3jmZaOCJ8"
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "active": true,
+     *       "url": "http://lorempixel.com/640/480/cats",
+     *       "description": "ItemDescription",
+     *       "name": "ItemName",
+     *       "_id": "551c31d0430d78991f5931e1"
+     *     }
+     */
     .post(function(req, res, next) {
 
       // create a new instance of the Media model
@@ -78,7 +97,24 @@ module.exports = function(router) {
 
   router.route('/media/:media_id')
 
-    // get the media with that id (accessed at GET http://localhost:port/api/media/:media_id)
+    /**
+     * @api {get} /media Get the media with that id
+     * @apiName Get
+     * @apiGroup Media
+     *
+     * @apiExample Example usage:
+     * curl -4 -i http://localhost:9000/api/media/551c31d0430d78991f5931e1 --header "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1NGVlNjE3NTQ2NWVhZWUzNWNkMjM3ZWQiLCJpYXQiOjE0Mjc4MTczNTksImV4cCI6MTQyNzgyMDk1OX0.M3BboY6U9RJlX1ulVG7e9xRVrVdY3qVhvp3jmZaOCJ8"
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "active": true,
+     *       "url": "http://lorempixel.com/640/480/cats",
+     *       "description": "ItemDescription",
+     *       "name": "ItemName",
+     *       "_id": "551c31d0430d78991f5931e1"
+     *     }
+     */
     .get(function(req, res, next) {
       Media.findById(req.params.media_id, function(err, model) {
 
@@ -95,7 +131,24 @@ module.exports = function(router) {
     })
 
 
-    // update the media with this id (accessed at PUT http://localhost:port/api/media/:media_id)
+    /**
+     * @api {put} /media Update the media with this id
+     * @apiName Update
+     * @apiGroup Media
+     *
+     * @apiExample Example usage:
+     * curl -4 -i -X PUT http://localhost:9000/api/media/551c31d0430d78991f5931e1 --data "name=ItemName&description=ItemDescription&active=false" --header "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1NGVlNjE3NTQ2NWVhZWUzNWNkMjM3ZWQiLCJpYXQiOjE0Mjc4MTczNTksImV4cCI6MTQyNzgyMDk1OX0.M3BboY6U9RJlX1ulVG7e9xRVrVdY3qVhvp3jmZaOCJ8"
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "active": false,
+     *       "url": "http://lorempixel.com/640/480/cats",
+     *       "description": "ItemDescription2",
+     *       "name": "ItemName2",
+     *       "_id": "551c31d0430d78991f5931e1"
+     *     }
+     */
     .put(function(req, res, next) {
 
       // use our media model to find the media we want
@@ -129,7 +182,24 @@ module.exports = function(router) {
     })
 
 
-    // delete the media with this id (accessed at DELETE http://localhost:port/api/media/:media_id)
+    /**
+     * @api {delete} /media Delete the media with this id
+     * @apiName Delete
+     * @apiGroup Media
+     *
+     * @apiExample Example usage:
+     * curl -4 -i -X DELETE http://localhost:9000/api/media/551c31d0430d78991f5931e1 --header "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1NGVlNjE3NTQ2NWVhZWUzNWNkMjM3ZWQiLCJpYXQiOjE0Mjc4MTczNTksImV4cCI6MTQyNzgyMDk1OX0.M3BboY6U9RJlX1ulVG7e9xRVrVdY3qVhvp3jmZaOCJ8"
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "active": true,
+     *       "url": "http://lorempixel.com/640/480/cats",
+     *       "description": "ItemDescription",
+     *       "name": "ItemName",
+     *       "_id": "551c31d0430d78991f5931e1"
+     *     }
+     */
     .delete(function(req, res, next) {
       var model = Media.findOne({
         _id: req.params.media_id
