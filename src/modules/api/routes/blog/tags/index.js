7f150afe28a0af3804a747664a1a894cc8b93ab0
josep ramon <jrcamps@gmail.com>
2015-07-10 20:53:24 +0200

    Update docs

--- src/modules/api/routes/blog/tags/index.js
+++ src/modules/api/routes/blog/tags/index.js
@@ -18,39 +18,44 @@ module.exports = function(router) {
 
 
     /**
-     * @apiDefine CommonApiParams
+     * @apiDefine Tags_CommonApiParams
      *
-     * @apiParam {String}  [expand]   Nested objects to expand. It can be an array.
+     * @apiParam {String}  [include]  Nested objects to expand. It can be an array.
      * @apiParam {Integer} [per_page] The methods that return multiple models are paginated by default. This determines
      *                                the number of elements returned (by default 20). There's a hard limit (200). Requests
      *                                with a greater value will return only the maximum allowed items.
      * @apiParam {Integer} [page]     The results page (for paginated results)
-     * @apiParam {String}  [sort_by]  Sort criteria. Accepts multiple values (arrays)
-     * @apiParam {String}  [order]    Sort direction. Accepted values: `1`, `-1`, `asc`, `desc`.
-     *                                It's applied to all the sort_by (because the backbone.paginator does not support this,
-     *                                anyway, this is really easy to change)
+     * @apiParam {String}  [order]    Sort criteria. Accepts multiple values (arrays or separated with commas).
+     *                                It also accepts the sort direction (if not provided, 'asc' will be used).
+     *                                Accepted values: `1`, `-1`, `asc`, `desc`.
+     *                                Examples:
+     *                                  `?order=id`
+     *                                  `?order=id|asc`
+     *                                  `?order=id|asc,name|desc`
+     *                                  `?order[]=id|asc&order[]=name
      *
      */
 
 
     /**
-     * @apiDefine CommonApiResponseHeader
+     * @apiDefine Tags_CommonApiResponseHeader
      *
      * @apiSuccess {Object} meta                 Response metadata
      * @apiSuccess {String} meta.url             Resource url
-     * @apiSuccess {Object} [meta.paginator]     Pagination params
      *
      */
 
 
     /**
-     * @apiDefine SingleEntityResponse
+     * @apiDefine Tags_SingleEntityResponse
      *
      * @apiSuccess {Object} data                 The Tag data
      * @apiSuccess {String} data.id              Id
      * @apiSuccess {String} data.name            Tag name
      * @apiSuccess {String} data.slug            URL slug
      * @apiSuccess {String} [data.description]   Tag description
+     * @apiSuccess {String} data.articles        Articles that have the tag applied. Collapsed by default (only a `meta` node with the collection url and the item count).
+     *                                           Can be expanded to the full tag objects (see the `include` parameter)
      * @apiSuccess {String} data.created_at      Creation date
      * @apiSuccess {String} data.updated_at      Last update date
      *
@@ -58,13 +63,16 @@ module.exports = function(router) {
 
 
     /**
-     * @apiDefine MultipleEntityResponse
+     * @apiDefine Tags_MultipleEntityResponse
      *
+     * @apiSuccess {Object} [meta.paginator]     Pagination params
      * @apiSuccess {Object[]} data               The Tags data
      * @apiSuccess {String} data.id              Id
      * @apiSuccess {String} data.name            Tag name
      * @apiSuccess {String} data.slug            URL slug
      * @apiSuccess {String} [data.description]   Tag description
+     * @apiSuccess {String} data.articles        Articles that have the tag applied. Collapsed by default (only a `meta` node with the collection url and the item count).
+     *                                           Can be expanded to the full tag objects (see the `include` parameter)
      * @apiSuccess {String} data.created_at      Creation date
      * @apiSuccess {String} data.updated_at      Last update date
      *
@@ -76,13 +84,13 @@ module.exports = function(router) {
      * @apiName List
      * @apiGroup BlogTags
      *
-     * @apiUse CommonApiParams
+     * @apiUse Tags_CommonApiParams
      *
      * @apiExample Example usage:
      * curl -4 -i curl -X GET -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMSIsImlhdCI6MTQzNDg0MTE5MywiZXhwIjoxNDM0ODQ0NzkzfQ.76hpdlVfAuBI59J1OTZ6y-nNGOkj7ZVOPgZagIJMxMQ" http://localhost:9000/api/blog/tags
      *
-     * @apiUse CommonApiResponseHeader
-     * @apiUse MultipleEntityResponse
+     * @apiUse Tags_CommonApiResponseHeader
+     * @apiUse Tags_MultipleEntityResponse
      *
      * @apiSuccessExample {json} Success-Response:
      *     HTTP/1.1 200 OK
@@ -102,14 +110,26 @@ module.exports = function(router) {
      *           "slug": "nesciunt",
      *           "updated_at": 1434807342,
      *           "created_at": 1434807342,
-     *           "id": "5585eeb41b7b3b821cb5ba5e"
+     *           "id": "5585eeb41b7b3b821cb5ba5e",
+     *           "articles": {
+     *             "meta": {
+     *               "url": "http://localhost:9000/api/blog/tags/5585eeb41b7b3b821cb5ba5e/articles",
+     *               "count": 4
+     *             }
+     *           }
      *         },
      *         {
      *           "name": "nostrum",
      *           "slug": "nostrum",
      *           "updated_at": 1434836605,
      *           "created_at": 1434836605,
-     *           "id": "5585eeb41b7b3b821cb5ba5f"
+     *           "id": "5585eeb41b7b3b821cb5ba5f",
+     *           "articles": {
+     *             "meta": {
+     *               "url": "http://localhost:9000/api/blog/tags/5585eeb41b7b3b821cb5ba5f/articles",
+     *               "count": 2
+     *             }
+     *           }
      *         }
      *       ]
      *     }
@@ -125,13 +145,13 @@ module.exports = function(router) {
      *
      * @apiParam {String} name           Tag name. (must be unique for this client)
      * @apiParam {String} [description}  Tag description.
-     * @apiUse CommonApiParams
+     * @apiUse Tags_CommonApiParams
      *
      * @apiExample Example usage:
      * curl -X POST -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMSIsImlhdCI6MTQzNDg0MTE5MywiZXhwIjoxNDM0ODQ0NzkzfQ.76hpdlVfAuBI59J1OTZ6y-nNGOkj7ZVOPgZagIJMxMQ" -H "Content-Type: application/x-www-form-urlencoded" -d 'name=foo&slug=foo&description=foo+description' http://localhost:9000/api/blog/tags
      *
-     * @apiUse CommonApiResponseHeader
-     * @apiUse SingleEntityResponse
+     * @apiUse Tags_CommonApiResponseHeader
+     * @apiUse Tags_SingleEntityResponse
      *
      * @apiSuccessExample {json} Success-Response:
      *     HTTP/1.1 200 OK
@@ -145,7 +165,13 @@ module.exports = function(router) {
      *         "name": "foo",
      *         "updated_at": 1434841419,
      *         "created_at": 1434841419,
-     *         "id": "5585f14bb3c67d5022b4262a"
+     *         "id": "5585f14bb3c67d5022b4262a",
+     *         "articles": {
+     *           "meta": {
+     *             "url": "http://localhost:9000/api/blog/tags/5585f14bb3c67d5022b4262a/articles",
+     *             "count": 0
+     *           }
+     *         }
      *       }
      *     }
      *
@@ -161,13 +187,13 @@ module.exports = function(router) {
      * @apiName Get
      * @apiGroup BlogTags
      *
-     * @apiUse CommonApiParams
+     * @apiUse Tags_CommonApiParams
      *
      * @apiExample Example usage:
      * curl -X GET -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMSIsImlhdCI6MTQzNDg0MTE5MywiZXhwIjoxNDM0ODQ0NzkzfQ.76hpdlVfAuBI59J1OTZ6y-nNGOkj7ZVOPgZagIJMxMQ" http://localhost:9000/api/blog/tags/5585eeb41b7b3b821cb5ba5e
      *
-     * @apiUse CommonApiResponseHeader
-     * @apiUse SingleEntityResponse
+     * @apiUse Tags_CommonApiResponseHeader
+     * @apiUse Tags_SingleEntityResponse
      *
      * @apiSuccessExample {json} Success-Response:
      *     HTTP/1.1 200 OK
@@ -180,7 +206,13 @@ module.exports = function(router) {
      *         "slug": "nesciunt",
      *         "updated_at": 1434807342,
      *         "created_at": 1434807342,
-     *         "id": "5585eeb41b7b3b821cb5ba5e"
+     *         "id": "5585eeb41b7b3b821cb5ba5e",
+     *         "articles": {
+     *           "meta": {
+     *             "url": "http://localhost:9000/api/blog/tags/5585eeb41b7b3b821cb5ba5e/articles",
+     *             "count": 0
+     *           }
+     *         }
      *       }
      *     }
      *
@@ -198,13 +230,13 @@ module.exports = function(router) {
      * @apiParam {String} [slug]         Slug. If not provided it will be autogenerated. If there's already some tag with the
      *                                   same slug, a numeric suffix will be added. For example, if the requested slug is *foo*
      *                                   and there's another tag with that slug, the slug for this tag will be *foo-1*
-     * @apiUse CommonApiParams
+     * @apiUse Tags_CommonApiParams
      *
      * @apiExample Example usage:
      * curl -X PUT -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMSIsImlhdCI6MTQzNDg0MTE5MywiZXhwIjoxNDM0ODQ0NzkzfQ.76hpdlVfAuBI59J1OTZ6y-nNGOkj7ZVOPgZagIJMxMQ" -H "Content-Type: application/x-www-form-urlencoded" -d 'name=bar&slug=bar&description=bar+description' http://localhost:9000/api/blog/tags/5585eeb41b7b3b821cb5ba5e
      *
-     * @apiUse CommonApiResponseHeader
-     * @apiUse SingleEntityResponse
+     * @apiUse Tags_CommonApiResponseHeader
+     * @apiUse Tags_SingleEntityResponse
      *
      * @apiSuccessExample {json} Success-Response:
      *     HTTP/1.1 200 OK
@@ -218,7 +250,13 @@ module.exports = function(router) {
      *         "slug": "bar",
      *         "updated_at": 1434842,
      *         "created_at": 1434807342,
-     *         "id": "5585eeb41b7b3b821cb5ba5e"
+     *         "id": "5585eeb41b7b3b821cb5ba5e",
+     *         "articles": {
+     *           "meta": {
+     *             "url": "http://localhost:9000/api/blog/tags/5585eeb41b7b3b821cb5ba5e/articles",
+     *             "count": 2
+     *           }
+     *         }
      *       }
      *     }
      *
@@ -231,13 +269,13 @@ module.exports = function(router) {
      * @apiName Delete
      * @apiGroup BlogTags
      *
-     * @apiUse CommonApiParams
+     * @apiUse Tags_CommonApiParams
      *
      * @apiExample Example usage:
      * curl -X DELETE -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMSIsImlhdCI6MTQzNDg0MTE5MywiZXhwIjoxNDM0ODQ0NzkzfQ.76hpdlVfAuBI59J1OTZ6y-nNGOkj7ZVOPgZagIJMxMQ" http://localhost:9000/api/blog/tags/5585eeb41b7b3b821cb5ba5e
      *
-     * @apiUse CommonApiResponseHeader
-     * @apiUse SingleEntityResponse
+     * @apiUse Tags_CommonApiResponseHeader
+     * @apiUse Tags_SingleEntityResponse
      *
      * @apiSuccessExample {json} Success-Response:
      *     HTTP/1.1 200 OK
@@ -249,7 +287,13 @@ module.exports = function(router) {
      *         "description": "bar description",
      *         "updated_at": 1434842,
      *         "created_at": 1434807342,
-     *         "id": "5585eeb41b7b3b821cb5ba5e"
+     *         "id": "5585eeb41b7b3b821cb5ba5e",
+     *         "articles": {
+     *           "meta": {
+     *             "url": "http://localhost:9000/api/blog/tags/5585eeb41b7b3b821cb5ba5e/articles",
+     *             "count": 0
+     *           }
+     *         }
      *       }
      *     }
      *
